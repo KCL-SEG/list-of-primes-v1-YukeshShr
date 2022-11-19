29022f1fdac82206b1e07e6684ee1fe2454f6a26
YukeshShr <yukeshr@hotmail.com>
2022-11-19 12:09:06 +0000

    Solve prime function

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    while(len(list) < number_of_primes):
+
+        for i in range(2, count):
+            if(count % i == 0):
+                break
+        else:
+            list.append(count)
+            
+        count += 1
     return list
